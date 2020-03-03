3e868618e7d8a8e18956e1120c35145a52bf274d
Simara Perez <sperez@decibelinsight.com>
2020-03-03 17:47:05 +0000

    Add filesize and pass it to clamd, after we tested the rest of the merge from original repo without this small change

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,9 +8,11 @@ else
 fi
 
 port="${CLAMD_PORT:-3310}"
+filesize="${MAXSIZE:-10GB}"
 
 echo "using clamd server: ${host}:${port}"
 
 # start in background
 
-java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host="${host}" --clamd.port="${port}"
+java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host="${host}" --clamd.port="${port}" --clamd.maxfilesize="${filesize}" --clamd.maxrequestsize="${filesize}"
+
